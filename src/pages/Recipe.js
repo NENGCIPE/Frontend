2d95f7fa671cd9d38fbbd572948582775420774a
Jang seungwoo <109260012+caffeineWoo@users.noreply.github.com>
2023-06-02 21:13:37 +0900

    youtube_api upload, recipe api_prepared

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,74 +1,84 @@
-import React, { useState } from 'react';
-//import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+// import React, { useState} from 'react';
+// import { useParams } from "react-router-dom";
 import './Recipe.css';
+import axios from 'axios';
+//사진 api로 받아와야함
+// ### Response
+
+// ingredients (array of objects): 냉장고에 저장된 재료 목록
+
+// - recipes_id (integer): 레시피 고유 ID
+// - recipes_name (string): 레시피의 이름
+// - member_ingred_id (integer) : 재료 id
+// - quantity (integer): 재료의 수량
+// - recipes_detail(string) : 레시피 설명
+
+// **<Success>**
+// json
+// {
+//  "ingredients": [{
+//      "recipes_id": 1,
+//      "recipes_name": "onion",
+// 		"Recipe" :  [{"member_ingred_id" : 1, "quantity" : 1}
+// 					 {"member_ingred_id" : 2, "quantity" : 5}],
+// 		"recipes_detail" : "dddddddd varchar~"
+// 	}]}
 
 
 function Recipe() {
+    //const apiEndpoint = 'https://nengcipe-server.store/api/recipes/recipe_id=';
+    const apiEndpoint = 'https://7d628e24-c60b-47c4-b882-a6a67df36e39.mock.pstmn.io';
 
-    //const { id } = useParams();
 
+
+    const [recipes_id, setRecipes_id] = useState('');
+    const [recipes_name, setRecipes_name] = useState('');
+    const [recipes_detail, setRecipes_detail] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const json =
+        {
+            "ingredients": [{
+                "recipes_id": 1,
+                "recipes_name": "onion",
+                "Recipe": [{ "member_ingred_id": 1, "quantity": 1 },
+                { "member_ingred_id": 2, "quantity": 5 }],
+                "recipes_detail": "dddddddd varchar~"
+            }]
+        }
+    const recipe = json.ingredients[0];//여기는 추후에 수정해야함
+
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: sessionStorage.getItem('jwt'),
+    };
+
+    const getRecipe = () => {
+
+        axios
+            .get(apiEndpoint, { headers })
+            .then((respone) => {//respone로 바꿔야 함
+                //const recipe = respone.ingredients[0];
+                setRecipes_id(recipe.recipes_id);
+                setRecipes_name(recipe.recipes_name);
+                setRecipes_detail(recipe.recipes_detail);
+                setIngredients(recipe.Recipe);
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    useEffect(() => {
+        getRecipe();
+    });
+
+    const [heartCount, setHeartCount] = useState(0);
     const [isAccordionOpen, setIsAccordionOpen] = useState(false);
     const [isHeartFull, setIsHeartFull] = useState(false);
-    const [heartCount, setHeartCount] = useState(0);
-
-    //const axios = require('axios');
-    //const apiKey = 'YOUR_API_KEY';
-    const apiEndpoint = 'https://nengcipe-server.store/api/recipes/recipe_id=';
-
-    fetch(apiEndpoint, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: sessionStorage.getItem('jwt'),
-        },
-        
-    })
-    .then((response) => response.json())
-    .then((result) => {
-        if (result.code === 200) {
-            const ingredients = result.ingredients;
-            if (ingredients.length > 0) {
-                const recipe = ingredients[0];
-                console.log('Recipe ID:', recipe.recipes_id);
-                console.log('Recipe Name:', recipe.recipes_name);
-                console.log('Recipe Detail:', recipe.recipes_detail);
-                console.log('Recipe Ingredients:');
-                recipe.Recipe.forEach((ingredient) => {
-                    console.log('Member Ingredient ID:', ingredient.member_ingred_id);
-                    console.log('Quantity:', ingredient.quantity);
-                });
-            } else {
-                console.log('No recipe found.');
-            }
-        } else if (result.code === 404) {
-            alert('Please recheck your ID or password.');
-        } else if (result.code === 401) {
-            alert('Please log in first')
-        }
-
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-    
 
 
 
-    // const recipe_name = [];
-    // const ingredients = [];
-    const description = [
-        "다진소고기는 키친타월에 핏물을 빼줍니다.",
-        "다진 돼지고기 역시 키친타월에 핏물을 빼주세요.",
-        "파는 곱게 다지세요.",
-        "양념장을 만들고 파를 섞어주세요.",
-        "고기는 서로 잘 섞어주세요.",
-        "섞은 고기에 양념장을 넣고 치댑니다.",
-        "(치대는것은 골고루 섞어서 반죽하듯이 하는것을 뜻해요-간이 잘배고 고기도 맛있어져요.)",
-        "고기는 먹기좋은 크기로 너무 두껍지 않게 만들어주세요. 가운데를 약간 옴폭파면 구울때 좋아요.",
-        "기름을 두르고 구우실때 센불로 하지마시고 중약불로 노릇하게 구워주세요.",
-        "다 구워지면 분량의 유장을 조금씩 위에 덧발라 약간 센불에서 윤기나게 구워주세요."
-    ];
-    
 
 
     const toggleAccordion = () => {
@@ -82,6 +92,7 @@ function Recipe() {
     };
 
 
+
     return (
         <div className='Recipe'>
             <div className='title'>
@@ -89,7 +100,7 @@ function Recipe() {
                     <h1>Recipe</h1>
                 </div>
                 <div className='dish_name'>
-                    <h1>떡갈비</h1>
+                    <h1>레시피 이름 : {recipes_name}</h1>
                 </div>
             </div>
 
@@ -97,50 +108,53 @@ function Recipe() {
                 <div className='food_img'>
                     <img className='food_img' src={`${process.env.PUBLIC_URL}/tteokgalbi.png`} alt="떡갈비" />
                 </div>
-                
+
                 <div className="dish_detail">
                     <div className='button_container'>
-                        
-                    <div className='recipe_name'>
-                            <h2>떡갈비</h2>
-                    </div>
+
+                        <div className='recipe_name'>
+                            <h2>레시피 이름 : {recipes_name}</h2><br /> 
+                            <h2>레시피 id : {recipes_id}</h2>
+                        </div>
+                        <div>
                             <button className='share_btn' onClick={toggleAccordion}>
                                 <img src={`${process.env.PUBLIC_URL}/share.png`} alt="공유" style={{ width: '30px', height: '30px' }} />
                             </button>
+
+
                             {isAccordionOpen && (
-                                <div className="accordion-content">
-                                    <p>공유하기</p>
+                                <div>
+                                    <p>Share</p>
+                                    {/* Additional content */}
                                 </div>
                             )}
-                            <button className='heart_btn' onClick={toggleHeart}>
-                                <img src={`${process.env.PUBLIC_URL}/${isHeartFull ? 'heart_full' : 'heart_empty'}.png`} alt="하트" style={{ width: '30px', height: '30px' }} />
-                                <span className="heart-count">{heartCount}</span>
-                            </button>
-                        
-                        
+
+                        </div>
+
+
+                        <button className='heart_btn' onClick={toggleHeart}>
+                            <img src={`${process.env.PUBLIC_URL}/${isHeartFull ? 'heart_full' : 'heart_empty'}.png`} alt="하트" style={{ width: '30px', height: '30px' }} />
+                            <span className="heart-count">{heartCount}</span>
+                        </button>
                     </div>
-                    
-
-
                     <div className='ingredients'>
                         <p>재료</p>
-                    </div>
-
-                    <div className='recipe-description'>
-                        <h3 >레시피 설명</h3>
-                        <ul>
-                            {description.map((step, index) => (
-                                <li key={index}>{step}</li>
-                            ))}
-                        </ul>
-                    </div>
-
-
+                        <h>{ingredients.map((ingredient) => (
+                            <span key={ingredient.member_ingred_id}>
+                                member_ingred_id: {ingredient.member_ingred_id} quantity: {ingredient.quantity}<br />
+                            </span>
+                        ))}</h><br />
                 </div>
+
+                <div className='recipe-description'>
+                    <h3 >레시피 설명</h3>
+                    <h>
+                        {recipes_detail}
+                    </h>
+                </div>
+
             </div>
         </div>
+        </div >
     );
-}
-
-
-export default Recipe;
+} export default Recipe;
