cd293eab651a844e3c698c555b2991aa91aa7366
L0CUST <namae01@naver.com>
2023-06-08 22:39:59 +0900

    스크랩 페이지 구현

--- src/pages/Scrap.js
+++ src/pages/Scrap.js
@@ -1,79 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Scrap.css'
+import Card from '../components/Card';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Scrap() {
+    const navigate = useNavigate();
+    const [scrapList, setScrapList] = useState([]);
+
+    useEffect(() => {
+        const getScrap = async () => {
+            const { data } = await axios.get("https://nengcipe-server.store/api/recipes/scrapList", {
+                headers: {
+                    Authorization: sessionStorage.getItem('jwt')
+                }
+            })
+            return data.result;
+        }
+        getScrap().then(result => setScrapList(result));
+    }, [])
+
     return (
         <div className='Scrap'>
-            <div className='title'>
-                <div className='page_name'>
-                    <h1>Scrap</h1>
-                </div>
-                <div className='scrap_name'>
-                    <h1>later</h1>
-                </div>
+            <div className='page_name'>
+                <h1>Scrap</h1>
             </div>
-
-            <div className='filter'>
-                <img src='/filter.png' alt='filter' />
-                <span>Filter</span>
-            </div>
-            <div className='line'>
-                <img src='/line.png' alt='line' />
-            </div>
-            <div className='checkboxes'>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Main dishes</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Side dishes</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Dessert</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Meat</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Fish</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Seafood</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Vegetable</span>
-                </div>
-                <div>
-                    <img className='checkbox' src='/box_empty.png' alt='box_empty' />
-                    <span>Dairy product</span>
-                </div>
-            </div>
-
-            <div className='search'>
-                <input type='text' placeholder='Search'/>
-                <img src='/search.png' alt='search' />
-            </div>
-
-            <div className='card_container'>
-                {[...Array(15)].map((_, i) => (
-                    <div key={i} className='card'>
-                        <img src='/tteokgalbi.png' alt='tteokgalbi' />
-                        <div className='card_info'>
-                            <span className='card_title'>떡갈비</span>
-                            <div className='card_icons'>
-                                <img className='heart' src='/heart_empty.png' alt='heart_empty' />
-                                <span className='ingredients'>재료</span>
-                            </div>
-                        </div>
+            <div className='scrap_list'>
+                {scrapList.map((item, index) => (
+                    <div className='scrap_container' onClick={() => navigate(`/recipe/${item.recipe.id}`)}>
+                        <Card img={item.recipe.imgUrl} title={item.recipe.recipeName} />
                     </div>
                 ))}
             </div>
-            
         </div>
     );
 }
