f389d7320d19f01efd1cff3d620ea6a853500c74
Jang seungwoo <109260012+caffeineWoo@users.noreply.github.com>
2023-06-09 15:00:20 +0900

    api delete

--- src/pages/Recommend.js
+++ src/pages/Recommend.js
@@ -18,7 +18,7 @@ const Recommend = () => {
 
 
     //gpt
-    const api_key = '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';
+    const api_key = 'sk-';
     const chatGPT = () => {
         setLoadingG(true);
 
@@ -62,7 +62,7 @@ const Recommend = () => {
                 'https://www.googleapis.com/youtube/v3/search',
                 {
                     params: {
-                        key: 'AIzaSyC8PWzJNCmAhHpWJ2IKKlaX25Dltp8S0Bc',
+                        key: '',
                         q: `${keywords}으로 만드는 자취생 레시피`,
                         type: 'video',
                         part: 'snippet'
