6162455c58fe4daebcb6d514df6d7d08944a4670
Jang seungwoo <109260012+caffeineWoo@users.noreply.github.com>
2023-06-16 15:44:55 +0900

    컴포넌트 오류 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
-    return (
-      <footer>
-        <div className="left-content">
-          &copy; Team 한사랑산학회
-        </div>
-        <div className="right-content">
-          이용약관 <span className="separator">|</span> 개인정보보호정책 <span className="separator">|</span> NENGCIPE소개
-        </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className="footer">
+      <div className="left-content">
+        &copy; Team 한사랑산학회
+      </div>
+      <div className="right-content">
+        이용약관 <span className="separator">|</span> 개인정보보호정책 <span className="separator">|</span> NENGCIPE소개
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
