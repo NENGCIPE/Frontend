6eac927e13e21cc511b13c2c822a6617143b63e0
L0CUST <namae01@naver.com>
2023-06-16 15:16:50 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -15,19 +15,6 @@ import UserGuide from "./pages/UserGuide";
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<div><Header/><Home/><Footer/></div>}/>
-    //   <Route path="/login" element={<div><LogIn/></div>}/>
-    //   <Route path="/signup" element={<div><SignUp/></div>}/>
-    //   <Route path="/nengjanggo" element={<div><Header/><Nengjanggo/></div>}/>
-    //   <Route path="/recipe/:recipeID" element={<div><Header/><Recipe/></div>}/>
-    //   <Route path="/recommend" element={<div><Header/><Recommend/></div>}/>
-    //   <Route path="/scrap" element={<div><Header/><Scrap/></div>}/>
-    //   <Route path="/mypage" element={<div><Header/><MyPage/></div>}/>
-    //   <Route path="/editmyinfo" element={<div><EditMyInfo/></div>}/>
-    //   <Route path="/UserGuide" element={<div><Header/><UserGuide/></div>}/>
-    //   <Route path="/oauth2" element={<KakaoRedirect/>}/>
-    // </Routes>
     <Routes>
     <Route path="/" element={<div><Header/><Home/><Footer/></div>}/>
     <Route path="/login" element={<div><LogIn/></div>}/>
